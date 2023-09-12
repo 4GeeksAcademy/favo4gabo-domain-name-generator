92967126bb59eadb58e8497f2415955ead2eda6f
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-09-12 22:15:58 +0000

    domain generator ready

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function () {
-  //write your code here
-  // console.log("Hello Rigo from the console!");
-  document.querySelector("#pizza").innerHTML = domainGenerator
-};
-
-const domainGenerator () => {
-  let pronoun = ['the','our'];
-  let adj = ['great', 'big' ];
-  let noun = ['jogger','racoon'];
-  
-  for (i = 0; i < 8; i++) {
-    console.log("i")
+let pronoun = ["the", "our"];
+let adjective = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".es"];
+for (let pro of pronoun) {
+  for (let adj of adjective) {
+    for (let no of noun) {
+      for (let dom of domain) {
+        console.log(pro + adj + no + dom);
+      }
+    }
   }
 }

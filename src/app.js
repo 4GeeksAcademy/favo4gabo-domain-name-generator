8d5d495f74c5a5eaf5ceb08ea8afde4bff403abe
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-09-12 18:22:43 +0000

    const y for

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // console.log("Hello Rigo from the console!");
+  document.querySelector("#pizza").innerHTML = domainGenerator
 };
+
+const domainGenerator () => {
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+  
+  for (i = 0; i < 8; i++) {
+    console.log("i")
+  }
+}
